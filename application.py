632c3561c61a21947a2913f5d01cea5f93af9a18
takamiya <dice7077@gmail.com>
2021-07-07 18:15:19 +0900

    remove main()

--- application.py
+++ application.py
@@ -1026,7 +1026,6 @@ def measure_saggital_length(saggital_data, reset_clicks):
         raise PreventUpdate
 
 
-def main():
-    application.run_server(debug=True, host='0.0.0.0')
+application.run_server(debug=True, host='0.0.0.0')
 
 
